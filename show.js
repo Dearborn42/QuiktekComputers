fe298decd016ea4b74af10ec4f1db55399b08f95
AndrewMurphy <amurf26@outlook.com>
2023-03-07 17:36:14 -0700

    media queried nav

--- show.js
+++ show.js
@@ -74,19 +74,12 @@ window.addEventListener("scroll", function() {
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
-  document.querySelector(".dropbtn").addEventListener("click", function () {
-  document.querySelector(".dropdown-content").classList.toggle("show");
-});
 }
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+     document.querySelector(".dropbtn").addEventListener("click", function () {
+        document.querySelector(".dropdown-content").classList.toggle("show");
+    });
+  }
+}
